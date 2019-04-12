92556dcbd6e83aa3cf45626c4d611f2a4397e788
George Rosenberger <gr2578@cumc.columbia.edu>
2019-04-12 08:13:08 -0400

    [FEATURE] Updated DIA WF

--- sjob.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -N SLOG
-#$ -cwd
-#$ -pe smp 1
-#$ -l mem=8G,time=8::
-
-snakemake --snakefile Snakefile.openswath -j 100 --latency-wait=30 --cluster-config envs/lsf.json --cluster "qsub -cwd -pe smp {cluster.nCPUs} -l mem={cluster.memory}M,time=8:0:0"
-
